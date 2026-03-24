a70cc16ecf5fd5d76ec517f42c52c25ed83b7de4
Preet Rana <preetrana1263@gmail.com>
2026-03-24 23:11:54 +0530

    made method for user model to create user

--- src/models/user.rs
+++ src/models/user.rs
@@ -3,7 +3,19 @@ use crate::enums::Role;
 struct User {
     id: i64,
     name: String,
-    emial: String,
+    email: String,
     password: String,
     role: Role,
+}
+
+impl User {
+    pub fn new(id: i64, name: String, email: String, password: String, role: Role) -> Self {
+        Self {
+            id,
+            name,
+            email,
+            password,
+            role,
+        }
+    }
 }
