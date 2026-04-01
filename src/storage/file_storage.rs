93d4e044b6b83ff5b400ad34066de268a90eaaa2
Preet Rana <preetrana1263@gmail.com>
2026-04-01 23:18:24 +0530

    added fuctionality to store data of complaints and announcements in file_storage.rs

--- src/storage/file_storage.rs
+++ src/storage/file_storage.rs
@@ -10,13 +10,53 @@ pub struct Storage {
 }
 
 impl Storage {
-    pub fn new(resident_file_path: &str,complaints_file_path: &str,announcements_file_path: &str) -> Self {
+    pub fn new(
+        resident_file_path: &str,
+        complaints_file_path: &str,
+        announcements_file_path: &str,
+    ) -> Self {
         Self {
             resident_file_path: resident_file_path.to_string(),
             announcements_file_path: announcements_file_path.to_string(),
             complaints_file_path: complaints_file_path.to_string(),
         }
     }
+
+    // Handle Complaint data
+
+    pub fn load_complaints(&self) -> Vec<Resident> {
+        if !Path::new(&self.complaints_file_path).exists() {
+            return Vec::new();
+        }
+
+        let data = fs::read_to_string(&self.complaints_file_path).unwrap_or("[]".to_string());
+
+        serde_json::from_str(&data).unwrap_or(Vec::new())
+    }
+
+    pub fn save_complaints(&self, complaints: &Vec<Resident>) {
+        let data = serde_json::to_string_pretty(complaints).unwrap();
+        fs::write(&self.complaints_file_path, data).expect("Unable to write in file");
+    }
+
+    // Handle Announcement data
+
+    pub fn load_announcements(&self) -> Vec<Resident> {
+        if !Path::new(&self.announcements_file_path).exists() {
+            return Vec::new();
+        }
+
+        let data = fs::read_to_string(&self.announcements_file_path).unwrap_or("[]".to_string());
+
+        serde_json::from_str(&data).unwrap_or(Vec::new())
+    }
+
+    pub fn save_announcements(&self, announcements: &Vec<Resident>) {
+        let data = serde_json::to_string_pretty(announcements).unwrap();
+        fs::write(&self.announcements_file_path, data).expect("Unable to write in file");
+    }
+
+    // Handle Resident data
 
     pub fn load_residents(&self) -> Vec<Resident> {
         if !Path::new(&self.resident_file_path).exists() {
